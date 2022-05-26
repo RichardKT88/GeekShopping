8c4f2f3b9b36dca6a469793881185eba07a100d1
RichardKT88 <kendy_t88@hotmail.com>
2022-05-26 11:52:58 -0300

    [Done] Seção 8: Organizando a Arquitetura dos Primeiros Microsserviços

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,14 +1,23 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Configurando conexão com o  banco de dados
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
-
 builder.Services.AddDbContext<MySQLContext>(options => options.
     UseMySql(connection, 
         new MySqlServerVersion(new Version(8,0,28))));
+//Configurando o AutoMapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//Adicionando o Repository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
