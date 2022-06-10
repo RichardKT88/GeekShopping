3b8931f55f66a0ef83b2ff453c6527a7765575d3
RichardKT88 <kendy_t88@hotmail.com>
2022-06-10 17:33:57 -0300

    [Progress]  Ajustando o HomeController para Permitir Exibir Detalhes do Produto

--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -8,5 +8,17 @@
         public string Description { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+
+        public string SubstringName()
+        {
+            if (Name.Length < 24) return Name;
+            return $"{Name.Substring(0, 21)} ...";
+        }  
+        
+        public string SubstringDescription()
+        {
+            if (Description.Length < 355) return Description;
+            return $"{Description.Substring(0, 352)} ...";
+        }
     }
 }
